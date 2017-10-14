98225dbccb926776c6f5963ab66aef6222d2bdcc
Yoon Wai Yan <waiyan@cloudcoder.com.my>
2017-10-14 12:42:37 +0800

    fix rspec errors

--- lib/blog/entities/article.rb
+++ lib/blog/entities/article.rb
@@ -3,6 +3,13 @@ require "types"
 module Blog
   module Entities
     class Article < Dry::Struct
+    	attribute :title, Types::Strict::String
+    	attribute :status, Types::ArticleStatus
+    	attribute :published_at, Types::Strict::Time.optional
+
+    	# title
+    	# status
+    	# published_at
     end
   end
 end
